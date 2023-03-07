3a1684a4311d3bb94d5d15dce2a1ba453a62cd49
John Chacpi <johnchacpi479@gmail.com>
2023-03-07 16:28:59 -0500

    changes on navbar styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,19 +17,19 @@ const Navbar = () => {
           <div>
             <ul className='hidden md:flex'>
               <Link href={"/"}>
-                <li className='ml-10 text-sm uppercase hover:border-b'>Home</li>
+                <li className='ml-10 text-sm uppercase hover:underline '>Home</li>
               </Link>
               <Link href={"/"}>
-                <li className='ml-10 text-sm uppercase hover:border-b'>About</li>
+                <li className='ml-10 text-sm uppercase hover:underline'>About</li>
               </Link>
               <Link href={"/"}>
-                <li className='ml-10 text-sm uppercase hover:border-b'>Skills</li>
+                <li className='ml-10 text-sm uppercase hover:underline'>Skills</li>
               </Link>
               <Link href={"/"}>
-                <li className='ml-10 text-sm uppercase hover:border-b'>Projects</li>
+                <li className='ml-10 text-sm uppercase hover:underline'>Projects</li>
               </Link>
               <Link href={"/"}>
-                <li className='ml-10 text-sm uppercase hover:border-b'>Contact</li>
+                <li className='ml-10 text-sm uppercase hover:underline'>Contact</li>
               </Link>
             </ul>
             <div className='md:hidden cursor-pointer' onClick={handleNav}>
