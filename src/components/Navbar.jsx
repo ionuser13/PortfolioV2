1356327f3c4d71c2cf8d22023cb8cfdca0a6eaf7
John Chacpi <johnchacpi479@gmail.com>
2023-03-08 10:34:48 -0500

    changed route and fixed mobile menu bug

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -66,20 +66,20 @@ const Navbar = () => {
             </div>
             <div className='py-4 flex flex-col'>
               <ul className='uppercase'>
-                <Link href={"/"}>
-                  <li className='py-4 text-sm'>Home</li>
+                <Link href={"/#home"}>
+                  <li onClick={() => {setNav(false)}} className='py-4 text-sm'>Home</li>
                 </Link>
-                <Link href={"/"}>
-                  <li className='py-4 text-sm'>About</li>
+                <Link href={"/#about"}>
+                  <li onClick={() => {setNav(false)}} className='py-4 text-sm'>About</li>
                 </Link>
-                <Link href={"/"}>
-                  <li className='py-4 text-sm'>Skills</li>
+                <Link href={"/#skills"}>
+                  <li onClick={() => {setNav(false)}} className='py-4 text-sm'>Skills</li>
                 </Link>
-                <Link href={"/"}>
-                  <li className='py-4 text-sm'>Projects</li>
+                <Link href={"/#projects"}>
+                  <li onClick={() => {setNav(false)}} className='py-4 text-sm'>Projects</li>
                 </Link>
-                <Link href={"/"}>
-                  <li className='py-4 text-sm'>Contact</li>
+                <Link href={"/#contact"}>
+                  <li onClick={() => {setNav(false)}} className='py-4 text-sm'>Contact</li>
                 </Link>
               </ul>
               <div className='pt-40'>
--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -12,7 +12,7 @@ const ProjectItem = ({title, technology, backgroundImg, projectUrl}) => {
         <div className='opacity-0 group-hover:opacity-100 absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] transition-all duration-75  ease-in'>
             <h3 className='text-2xl text-white tracking-wider text-center'>{title}</h3>
             <p className='pb-4 pt-2 text-white text-center'>{technology}</p>
-            <Link href={"/"}>
+            <Link href={projectUrl}>
                 <p className='text-center py-3 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer'>More info</p>
             </Link>
         </div>
