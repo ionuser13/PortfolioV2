66c0693f6fc9dd129b809f2d082b9d4ef19dc16a
John Chacpi <johnchacpi479@gmail.com>
2023-03-09 17:58:07 -0500

    added bg image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,6 @@
 import Head from 'next/head'
-import Image from 'next/image'
-
 import Main from '@/components/Main';
 import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
 import About from '@/components/About';
 import Skills from '@/components/Skills';
 import Projects from '@/components/Projects';
