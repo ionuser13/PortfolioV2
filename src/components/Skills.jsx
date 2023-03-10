7e573ee57db1b3634563fad8a7b1bad0d8d78cc0
John Chacpi <johnchacpi479@gmail.com>
2023-03-09 19:29:38 -0500

    changed way to link images in skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,6 +1,16 @@
 import React from 'react';
-import Image from 'next/image';
+import htmlImage from "@public/assets/skills/html.png";
+import cssImage from "@public/assets/skills/css.png";
+import bootstrapImage from "@public/assets/skills/bootstrap.png";
+import tailwindImage from "@public/assets/skills/tailwind.png";
+import sassImage from "@public/assets/skills/sass.png";
+import jsImage from "@public/assets/skills/javascript.png"
+import reactImage from "@public/assets/skills/react.png"
+import nextImage from "@public/assets/skills/nextjs.png";
+import gitImage from "@public/assets/skills/git.png";
+import githubImage from "@public/assets/skills/github1.png";
 import IndividualSkill from './IndividualSkill';
+
 
 const Skills = () => {
   return (
@@ -10,16 +20,16 @@ const Skills = () => {
             <h2 className='py-4'>What can I do?</h2>
             <div className='grid md:grid-cols-2 lg:grid-flow-cols-4 gap-8'>
 
-                <IndividualSkill source={"/../public/assets/skills/html.png"} skillName={"HTML5"}/>
-                <IndividualSkill source={"/../public/assets/skills/css.png"} skillName={"CSS3"}/>
-                <IndividualSkill source={"/../public/assets/skills/bootstrap.png"} skillName={"Bootstrap"}/>
-                <IndividualSkill source={"/../public/assets/skills/tailwind.png"} skillName={"Tailwind CSS"}/>
-                <IndividualSkill source={"/../public/assets/skills/sass.png"} skillName={"SASS"}/>
-                <IndividualSkill source={"/../public/assets/skills/javascript.png"} skillName={"JavaScript"}/>
-                <IndividualSkill source={"/../public/assets/skills/react.png"} skillName={"React JS"}/>
-                <IndividualSkill source={"/../public/assets/skills/nextjs.png"} skillName={"Next JS"}/>
-                <IndividualSkill source={"/../public/assets/skills/git.png"} skillName={"Git"}/>
-                <IndividualSkill source={"/../public/assets/skills/github1.png"} skillName={"GitHub"}/>
+                <IndividualSkill source={htmlImage} skillName={"HTML5"}/>
+                <IndividualSkill source={cssImage} skillName={"CSS3"}/>
+                <IndividualSkill source={bootstrapImage} skillName={"Bootstrap"}/>
+                <IndividualSkill source={tailwindImage} skillName={"Tailwind CSS"}/>
+                <IndividualSkill source={sassImage} skillName={"SASS"}/>
+                <IndividualSkill source={jsImage} skillName={"JavaScript"}/>
+                <IndividualSkill source={reactImage} skillName={"React JS"}/>
+                <IndividualSkill source={nextImage} skillName={"Next JS"}/>
+                <IndividualSkill source={gitImage} skillName={"Git"}/>
+                <IndividualSkill source={githubImage} skillName={"GitHub"}/>
 
             </div>
         </div>
